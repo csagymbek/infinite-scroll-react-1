020329ff1ed6062b220dfb5d3a9cdce61dee3564
Serik Sagymbek <csagymbek@gmail.com>
2020-09-27 01:49:17 -0700

    second commit

---
+++ README.md
@@ -0,0 +1,1 @@
+### Infinite Scrolling With React
---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, {useState, useRef, useCallback} from 'react';
+import useBookSearch from "./useBookSearch";
+
+export default function App() {
+  const [query, setQuery] = useState("");
+  const [pageNumber, setPageNumber] = useState(1);
+  const {loading, error, books, hasMore} = useBookSearch(query, pageNumber);
+  const observer = useRef();
+  const lastBookRef = useCallback(node => {
+
+    if(loading) return;
+    if(observer.current) observer.current.disconnect();
+    observer.current =  new IntersectionObserver(entries => {
+      if(entries[0].isIntersecting && hasMore){
+        setPageNumber(prevPageNumber => prevPageNumber + 1);
+      }
+    });
+    if(node) observer.current.observe(node);
+  }, [loading, hasMore]);
+
+  function handleSearch(e){
+    setQuery(e.target.value);
+    setPageNumber(1);
+  }
+
+  return (
+      <>
+        <input type="text" value={query} onChange={handleSearch}/>
+        {books.map((book, index) => {
+          if(books.length === index + 1){
+            return <div key={book} ref={lastBookRef}>{book}</div>
+          } else{
+            return <div key={book}>{book}</div>
+          }
+        })}
+        <div>{loading && "Loading..."}</div>
+        <div>{error && "Error"}</div>
+      </>
+  );
+}
